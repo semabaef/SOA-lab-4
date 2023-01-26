94de37cde7eea670d885f76d87208a6acda30afe
Shibaev Sema <seemon.shibaev@yandex.ru>
2023-01-26 21:13:49 +0300

    c богом!

--- VehiclesCollectionService/src/main/java/configuration/VehiclesApplication.java
+++ VehiclesCollectionService/src/main/java/configuration/VehiclesApplication.java
@@ -5,23 +5,5 @@ import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class VehiclesApplication extends Application {
-//    @Override
-//    public Set<Class<?>> getClasses() {
-//        Set<Class<?>> resources = new HashSet<>();
-//
-//        resources.add(CorsFilter.class);
-//        resources.add(HttpApplicationExceptionMapper.class);
-//        resources.add(ShopVehiclesResourse.class);
-//        resources.add(VehiclesCollectionCrudResource.class);
-//        resources.add(VehiclesCollectionExtraOperationsResource.class);
-//        resources.add(VehicleNoIdMessageBodyReader.class);
-//        resources.add(VehicleWithIdMessageBodyReader.class);
-//        resources.add(ListVehicleMessageBodyWriter.class);
-//        resources.add(VehicleMessageBodyWriter.class);
-//        resources.add(VehiclesCountMessageBodyWriter.class);
-//        resources.a
-//
-//        return resources;
-//
-//    }
+
 }
